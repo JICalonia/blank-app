2c0d9310c1fa20bda8196773f2332cb2d5d12818
JICalonia <jiCalonia@mcm.edu.ph>
2026-09-17 01:20:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,5 @@ import streamlit as st
 
 st.title("🎈 datto-san's Practice Playground")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).
-
-    If not, feel free to [watch this video](https://youtube.com/watch?v=dQw4w9WgXcQ) for more information."
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
