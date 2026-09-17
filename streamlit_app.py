913ff30728e5c3e4b2b4f0f0da5b48ae0a062a39
JICalonia <jiCalonia@mcm.edu.ph>
2026-09-17 14:37:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ st.title("💙 datto-san's Practice Playground 👾")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/). If not, then [watch this video](https://www.youtube.com/watch?v=dQw4w9WgXcQ) for more information."
 )
+
+st.button("Say hi to the datCoolPro")
+
 st.write(
     "[Support Me](https://ko-fi.com/datcoolpro101)"
 )
