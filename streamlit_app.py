a0c3461e79cf3af8feb368732016698083b949cc
JICalonia <jiCalonia@mcm.edu.ph>
2026-09-17 13:44:34 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/). If not, then [watch this video](https://www.youtube.com/watch?v=dQw4w9WgXcQ) for more information."
 )
 st.write(
-    "[Support Me](https://datcoolpro101.carrd.co/)"
+    "[Support Me](https://ko-fi.com/datcoolpro101)"
 )
